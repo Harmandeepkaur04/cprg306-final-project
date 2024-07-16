3e28fee3534ef71dff4b5180f802ceb60957e486
simranjotbal <simranjotkaur.bal@edu.sait.ca>
2024-07-16 12:20:51 -0600

    testing

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-blue-50">
       This is Web Dev 02 Final project
     </main>
   );
