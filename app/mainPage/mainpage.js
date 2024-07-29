cf3569db9313e5e12d8b7d3aec4ad08f069cd378
Harmandeep_Kaur <Harmandeep.Kaur04@edu.sait.ca>
2024-07-28 19:55:28 -0600

    Modified the Repo cleared unnecessary files and made it cleaner
    Cleaned the files to increase the readability and easier to understand also made an empty stylesheet to put the CSS

--- app/mainPage/mainpage.js
+++ app/mainPage/mainpage.js
@@ -2,8 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-//import './styles.css';
-
+import './styles.css';
 const CurrentWeatherPage = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import HomePage from "./pages/homePage";
 import WeatherPage from "./mainPage/mainpage";
 
 export default function Home() {
